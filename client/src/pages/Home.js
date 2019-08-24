3debad6f72647e0bf02ae3b16f4294eca4796396
Will <whof@bu.edu>
2019-08-23 22:09:39 -0400

    home page search bar sets state

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import axios from "axios";
-import { List, ListItem } from "../components/bookreturn"
+// import { List, ListItem } from "../components/bookreturn"
 class Home extends Component {
     state = {
         Books: [],
@@ -11,6 +11,12 @@ class Home extends Component {
 
     saveBook = (data) => {
         axios.post("/api/addBook", data)
+    }
+    handleInputChange = e => {
+        console.log(e.target)
+        this.setState({
+            iTitle: e.target.value
+        })
     }
 
     render() {
@@ -23,7 +29,7 @@ class Home extends Component {
                         <div class="input-group-prepend">
                             <span class="input-group-text" id="inputGroup-sizing-sm">Search</span>
                         </div>
-                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" />
+                        <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-sm" onChange={this.handleInputChange} value={this.state.iTitle} />
                     </div>
                 </div>
             </div>
